6a5503798d581371015e627a55d233f67149e9ca
ZhuChongLin <8210856@qq.com>
2024-03-19 18:07:58 +0800

    找不到类、方法、函数抽异常象类

---
+++ src/Exception/NotFoundExceptionAbstract.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Core\Exception;
+
+use Psr\Container\NotFoundExceptionInterface;
+use Throwable;
+
+/**
+ * 容器依赖注入反射执行函数、类不存在时应当抛出的异常
+ */
+abstract class NotFoundExceptionAbstract extends ContainerException implements NotFoundExceptionInterface
+{
+  public function __construct(string $message, protected string $name, Throwable $previous = null)
+  {
+    $this->message = $message;
+    parent::__construct($message, -404, $previous);
+  }
+
+  /**
+   * 获取方法名或类名
+   * @access public
+   * @return string
+   */
+  public function getName(): string
+  {
+    return $this->name;
+  }
+}
