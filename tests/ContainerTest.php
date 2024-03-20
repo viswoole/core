bb2ae34db768fb89f1d399f109df01b3c058edc3
ZhuChongLin <8210856@qq.com>
2024-03-20 12:18:11 +0800

    新增更多测试方法

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -20,11 +20,15 @@ use ViSwoole\Core\Container;
 
 class A
 {
-
+  public string $test;
 }
 
 class B
 {
+  public function __construct(A $a)
+  {
+  }
+
   public function test(A $a): A
   {
     return $a;
@@ -46,5 +50,70 @@ class ContainerTest extends TestCase
     });
     // 测试make方法并断言输出值
     self::assertEquals('func', Container::single()->make('func'));
+  }
+
+  /**
+   * 测试反射调用方法
+   *
+   * @return void
+   */
+  public function testInvokeMethod()
+  {
+    $result = Container::single()->invokeMethod([B::class, 'test']);
+    self::assertInstanceOf(A::class, $result);
+  }
+
+  /**
+   * 测试反射调用函数
+   *
+   * @return void
+   */
+  public function testInvokeFunc()
+  {
+    $result = Container::single()->invokeFunction(function (A $a, B $b) {
+      return 'test';
+    }, ['a' => new A()]);
+    self::assertEquals('test', $result);
+    $result = Container::single()->invokeFunction(function (A $a, B $b) {
+      return 'test';
+    });
+    self::assertEquals('test', $result);
+  }
+
+  /**
+   * 测试反射调用类实例化
+   *
+   * @return void
+   */
+  public function testInvokeClass()
+  {
+    $result = Container::single()->invokeClass(B::class);
+    self::assertInstanceOf(B::class, $result);
+  }
+
+  /**
+   * 测试注册回调和监听
+   *
+   * @return void
+   */
+  public function testCallback()
+  {
+    $callback = function (A $a, Container $container) {
+      $a->test = 'test';
+    };
+    Container::single()->resolving(A::class, $callback);
+    $a = Container::single()->invokeClass(A::class);
+    static::assertEquals('test', $a->test);
+
+    // 测试注册通用回调
+    Container::single()->resolving($callback);
+    $a = Container::single()->invokeClass(A::class);
+    static::assertEquals('test', $a->test);
+
+    // 测试删除回调
+    Container::single()->removeCallback(A::class, $callback);
+    Container::single()->removeCallback('*', $callback);
+    $a = Container::single()->invokeClass(A::class);
+    static::assertFalse(isset($a->test));
   }
 }
