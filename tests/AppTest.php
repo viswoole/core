60d3eee38b78143858bcdf51b9550c6c925c1b38
ZhuChongLin <8210856@qq.com>
2024-03-20 11:25:41 +0800

    测试APP容器实例化

---
+++ tests/AppTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use ViSwoole\Core\App;
+
+class AppTest extends TestCase
+{
+  public function testApp()
+  {
+    $container = new App();
+    $container->bind('test', function () {
+      return 'test';
+    });
+    self::assertEquals('test', $container->make('test'));
+  }
+}
