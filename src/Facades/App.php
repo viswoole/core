df71470929b6c6d109eafe70859b9a3f2e5143c8
ZhuChongLin <8210856@qq.com>
2024-03-23 18:52:52 +0800

    更新注释

--- src/Facades/App.php
+++ src/Facades/App.php
@@ -15,9 +15,32 @@ declare (strict_types=1);
 
 namespace ViSwoole\Core\Facades;
 
+use Closure;
 use Override;
+use ViSwoole\Core\App as Container;
 use ViSwoole\Core\Facade;
 
+/**
+ * App基础容器Facade类
+ *
+ * @method static void bind(array|string $abstract, mixed $concrete) 注册服务到容器中, 支持批量注册。
+ * @method static Container single() 获取容器单实例
+ * @method static mixed invokeFunction(Closure|string $function, array $vars) 执行闭包函数，支持依赖参数注入。
+ * @method static mixed make(string $abstract, array $vars) 获取容器中的服务，已经存在则直接获取。
+ * @method static mixed invoke(callable|string $callable, array $vars) 调用反射执行函数、匿名函数、以及类或方法，支持依赖注入。
+ * @method static mixed invokeMethod(array|string $method, array $vars) 调用反射执行类的方法，支持依赖注入。
+ * @method static object invokeClass(string $class, array $vars) 调用反射执行类的实例化，支持依赖注入。
+ * @method static void resolving(Closure|string $abstract, ?Closure $callback) 注册一个解析事件回调
+ * @method static void removeCallback(Closure|string $abstract, ?Closure $callback) 删除解析事件回调
+ * @method static object get(string $id) 获取容器中的对象实例
+ * @method static bool hasBind(string $abstract) 判断标识或接口是否已经绑定
+ * @method static bool has(string $id) 通过标识或接口类名判断是否已经绑定或注册单例
+ * @method static bool exists(string $abstract) 判断容器中是否注册单实例
+ * @method static void remove(string $abstract) 删除容器中的服务实例
+ * @method static int count() 获取容器中实例的数量
+ *
+ * 优化命令：php viswoole optimize:facade ViSwoole\\Core\\Facades\\App
+ */
 class App extends Facade
 {
 
@@ -29,6 +52,6 @@ class App extends Facade
    */
   #[Override] protected static function getFacadeClass(): string
   {
-    return App::class;
+    return Container::class;
   }
 }
