ab559668f76ef32dd901a461fc15338eca482bcd
ZhuChongLin <8210856@qq.com>
2024-03-24 14:41:54 +0800

    更新注释

--- src/Facades/Console.php
+++ src/Facades/Console.php
@@ -16,8 +16,19 @@ declare (strict_types=1);
 namespace ViSwoole\Core\Facades;
 
 use Override;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use ViSwoole\Core\Facade;
 
+/**
+ * 命令行处理程序
+ *
+ * @method static int run(?InputInterface $input, ?OutputInterface $output) Runs the current application.
+ * @method static void addCommands(array $commands) 注册多个处理程序实例
+ * @method static void addCommand(Command $command) 注册一个处理程序实例
+ * 优化命令：php viswoole optimize:facade ViSwoole\\Core\\Facades\\Console
+ */
 class Console extends Facade
 {
 
