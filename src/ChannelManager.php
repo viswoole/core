7ce1f04137086d97ed6b7f59df2af900a2bd9c97
ZhuChongLin <8210856@qq.com>
2024-04-10 15:21:21 +0800

    通道连接池管理器

---
+++ src/ChannelManager.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Core;
+
+use Override;
+use ViSwoole\Core\Contract\ChannelManagerInterface;
+use ViSwoole\Core\Contract\ConnectionPoolInterface;
+use ViSwoole\Core\Exception\ChannelNotFoundException;
+use function Swoole\Coroutine\run;
+
+/**
+ * 连接池管理器基类
+ */
+abstract class ChannelManager implements ChannelManagerInterface
+{
+  /**
+   * @var array{string:ConnectionPoolInterface}
+   */
+  protected array $channels = [];
+  /**
+   * @var string 默认连接池
+   */
+  protected string $defaultChannel;
+
+  /**
+   * @param array $channels 通道名称
+   * @param string $defaultChannel
+   */
+  public function __construct(
+    array  $channels,
+    string $defaultChannel
+  )
+  {
+    $this->defaultChannel = $defaultChannel;
+    foreach ($channels as $name => $config) {
+      run(function () use ($name, $config) {
+        $connect = $this->createPool($config);
+        $this->addChannel($name, $connect);
+      });
+    }
+  }
+
+  /**
+   * 创建连接池
+   *
+   * @param mixed $config 配置
+   * @return ConnectionPoolInterface
+   */
+  abstract protected function createPool(mixed $config): ConnectionPoolInterface;
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function addChannel(string $name, ConnectionPoolInterface $channel): void
+  {
+    $this->channels[strtolower($name)] = $channel;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function getChannel(?string $channel_name = null): ConnectionPoolInterface
+  {
+    if (empty($this->channels)) throw new ChannelNotFoundException('通道列表为空');
+    if (empty($channel_alias)) $channel_alias = $this->defaultChannel;
+    if ($this->hasChannel($channel_alias)) {
+      return $this->channels[strtolower($channel_alias)];
+    } else {
+      throw new ChannelNotFoundException("通道{$channel_alias}不存在");
+    }
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function hasChannel(string $channel_name): bool
+  {
+    return isset($this->channels[strtolower($channel_name)]);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function setDefaultChannel(string $channel_name): void
+  {
+    if (!$this->hasChannel($channel_name)) throw new ChannelNotFoundException(
+      "redis通道{$channel_name}不存在"
+    );
+    $this->defaultChannel = $channel_name;
+  }
+}
---
+++ src/Contract/ChannelManagerInterface.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Core\Contract;
+
+use ViSwoole\Core\Exception\ChannelNotFoundException;
+
+/**
+ * 连接池通道管理接口
+ */
+interface ChannelManagerInterface
+{
+  /**
+   * 获取通道
+   *
+   * @param string|null $channel_name 如果传入null则为获取默认通道连接池
+   * @return ConnectionPoolInterface 返回连接池
+   * @throws ChannelNotFoundException 通道不存在时抛出异常
+   */
+  public function getChannel(?string $channel_name = null): ConnectionPoolInterface;
+
+  /**
+   * 判断通道是否存在
+   *
+   * @param string $channel_name 不区分大小写的通道名称
+   * @return bool
+   */
+  public function hasChannel(string $channel_name): bool;
+
+  /**
+   * 设置/更改默认通道
+   *
+   * @access public
+   * @param string $channel_name 通道名称，不区分大小写
+   * @return void
+   * @throws ChannelNotFoundException 通道不存在时抛出异常
+   */
+  public function setDefaultChannel(string $channel_name): void;
+
+  /**
+   * 添加通道
+   *
+   * @param string $name 通道名称
+   * @param ConnectionPoolInterface $channel 连接池
+   * @return void
+   */
+  public function addChannel(string $name, ConnectionPoolInterface $channel): void;
+}
