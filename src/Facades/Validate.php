3db1a06e5ef18166369f332948fefba3bc5ecb04
ZhuChongLin <8210856@qq.com>
2024-04-14 17:38:05 +0800

    重构门面单例验证器

--- src/Facades/Validate.php
+++ src/Facades/Validate.php
@@ -15,7 +15,6 @@ declare (strict_types=1);
 
 namespace ViSwoole\Core\Facades;
 
-use Closure;
 use Override;
 use ViSwoole\Core\Contract\ValidateInterface;
 use ViSwoole\Core\Facade;
@@ -24,11 +23,6 @@ use ViSwoole\Core\Facade;
  * @method static bool check(array $data, bool $batch = false) 数据验证
  * @method static ValidateInterface rules(array $rules) 设置验证规则
  * @method static ValidateInterface message(array $message) 设置自定义提示
- * @method static ValidateInterface scene(string $name) 设置验证场景
- * @method static string|false hasScene(string $name) 判断是否存在某个验证场景
- * @method static ValidateInterface only(array|string $fields) 指定需要验证的字段列表
- * @method static ValidateInterface remove(array|string $field, array|string|null $rule = null) 移除验证规则
- * @method static ValidateInterface append(array|string $field, Closure|array|string|null $rule = null) 追加某个字段的验证规则
  *
  * 优化命令：php viswoole optimize:facade ViSwoole\\Core\\Facades\\Validate
  */
@@ -44,15 +38,5 @@ class Validate extends Facade
   #[Override] protected static function getFacadeClass(): string
   {
     return \ViSwoole\Core\Validate::class;
-  }
-
-  /**
-   * 重写createFacade方法，直接返回实例，不执行容器反射，提高效率
-   *
-   * @return \ViSwoole\Core\Validate
-   */
-  #[Override] protected static function createFacade(): object
-  {
-    return new \ViSwoole\Core\Validate();
   }
 }
