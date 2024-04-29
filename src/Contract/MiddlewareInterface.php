6892c783b8e9dc0ec86bd307bd4addac0cae8336
ZhuChongLin <8210856@qq.com>
2024-04-29 15:14:52 +0800

    迁移路由

---
+++ src/Contract/MiddlewareInterface.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Core\Contract;
+
+
+use Closure;
+use ViSwoole\HttpServer\Contract\RequestInterface;
+
+interface MiddlewareInterface
+{
+  /**
+   * 中间件处理方法
+   *
+   * @param RequestInterface $request 请求对象
+   * @param Closure $handler 下一个处理程序
+   * @return mixed
+   */
+  public function process(
+    Closure $handler
+  ): mixed;
+}
---
+++ src/Service/RouterService.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *  +----------------------------------------------------------------------
+ *  | ViSwoole [基于swoole开发的高性能快速开发框架]
+ *  +----------------------------------------------------------------------
+ *  | Copyright (c) 2024
+ *  +----------------------------------------------------------------------
+ *  | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+ *  +----------------------------------------------------------------------
+ *  | Author: ZhuChongLin <8210856@qq.com>
+ *  +----------------------------------------------------------------------
+ */
+
+declare (strict_types=1);
+
+namespace ViSwoole\Core\Service;
+
+use Override;
+use ViSwoole\Core\Facades\Event;
+use ViSwoole\Core\Middleware;
+use ViSwoole\Core\Router;
+use ViSwoole\Core\Server;
+use ViSwoole\Core\ServiceProvider;
+
+class RouterService extends ServiceProvider
+{
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function boot(): void
+  {
+    // 监听服务启动之前
+    Event::on('ServerStartBefore', function (Server $server) {
+      if ($server->getConfig()['type'] === \Swoole\Http\Server::class) {
+        Middleware::init();
+        Router::factory();
+      }
+    });
+  }
+
+  /**
+   * @inheritDoc
+   */
+  #[Override] public function register(): void
+  {
+    $this->app->bind('router', Router::class);
+  }
+}
